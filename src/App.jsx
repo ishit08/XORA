9b9a795d59c925d42c1635ca30e3bf18614a501d
Ishit Subhram <ishit21csu041@ncuindia.edu>
2024-11-16 12:27:34 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Header from "./sections/Header.jsx";
+
+const App = () => {
+    return (
+        <main clasNamne="overflow-hidden">
+
+            <Header />
+        </main>
+
+    )
+}
+export default App
