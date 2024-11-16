2592a0b1a167b4f96cccf37ed7467ad29179651d
Ishit Subhram <ishit21csu041@ncuindia.edu>
2024-11-16 13:02:25 +0530

    Hero section updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import Header from "./sections/Header.jsx";
+import Hero from "./sections/Hero.jsx";
 
 const App = () => {
-    return (
-        <main clasNamne="overflow-hidden">
-
-            <Header />
-        </main>
-
-    )
-}
-export default App
+  return (
+    <main className="overflow-hidden">
+      <Header />
+      <Hero />
+    </main>
+  );
+};
+export default App;
